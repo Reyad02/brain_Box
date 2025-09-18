863b5c06cf4baa34549a2d2344b415c4c26397a5
Sonjoy Dey <95380764+sonjoy2074@users.noreply.github.com>
2025-09-18 12:04:27 +0600

    create basic NCLEX -RN topic related quetion creation

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-print("Testing the Project")
+from google import genai
+from dotenv import load_dotenv
+import os
+load_dotenv()
+client = genai.Client()
+
+response = client.models.generate_content(
+    model="gemini-2.5-flash",
+    contents="Write 20 practice questions on the topic NCLEX -RN pharmacology with answers",
+)
+
+print(response.text)
